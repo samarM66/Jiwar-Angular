437957d4024aaf489cbef525e581a5c5608d5c65
Hagar Bastawi <jp.hagarbastawi@gmail.com>
2025-12-16 11:24:00 +0200

    edit router import

--- app/src/app/app.config.ts
+++ app/src/app/app.config.ts
@@ -1,8 +1,8 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
 import { router } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(router)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
+    router
+  ]
 };
